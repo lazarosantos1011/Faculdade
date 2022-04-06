2b702fc617e092f3da3b49445909de3ea51464ef
Lázaro Santos <98424392+lazarosantos1011@users.noreply.github.com>
2022-04-06 10:32:02 -0300

    Update Pisando_em_ovos.py

--- Pisando_em_ovos.py
+++ Pisando_em_ovos.py
@@ -1,5 +1,7 @@
 #Pisando em ovos
 
+import numpy as np
+
 print("Sejam bem-vindos ao pisando em ovos!\n")
 def menu():
     print('Opções:\n1 - Definir Armador\n2 - Plantar Armadilhas\n3 - Iniciar com Andarilho\n4 - Mostrar o placar\n0 - Finalizar o Jogo')
@@ -10,7 +12,7 @@ while True:
     if opcoesMenu == 1:
         print("\nQual jogador plantará as armadilhas? [1 ou 2]")
         armador = int(input())
-
+            
         if armador == 1:   
             andarilho = 2
         elif armador == 2:
@@ -19,7 +21,22 @@ while True:
         menu()
 
     if opcoesMenu == 2:
-        print("\nEm desenvolvimento...1\n")
+        A = "A"
+        colunas = [0,1,2,3,4,5,6,7]
+        l2 = [1, A,A,A,A,A,A,A]
+        l3 = [2, A,A,A,A,A,A,A]
+        l4 = [3, A,A,A,A,A,A,A]
+        l5 = [4, A,A,A,A,A,A,A]    
+        l6 = [5, A,A,A,A,A,A,A]
+        l7 = [6, A,A,A,A,A,A,A] 
+        l8 = [7, A,A,A,A,A,A,A]
+        terreno = []
+        terreno.append([colunas, l2, l3, l4, l5, l6, l7, l8])
+        print(terreno)
+
+        print(f"Jogador {armador}, você pode esconder até 3 ovos podres por linha do terreno")
+        colunaEscolhida = int(input("Em qual coluna da linha 1 você quer esconder ovos podres? [1 a 7]"))
+
         menu()
 
     if opcoesMenu == 3:
@@ -28,10 +45,10 @@ while True:
 
     if opcoesMenu == 4:
         print("\nEm desenvolvimento...3\n")
-        print("Pontuação do jogador 1: WIP\nPontuação do jogador 2: WIP")
+        print("Pontuação do jogador 1: WIP\nPontuação do jogador 2: WIP\n")
         menu()
 
     if opcoesMenu == 0:
         print("\nJogo encerrado.\n")
-        print("Pontuação final do jogador 1: \nPontuação final do jogador 2: ")
+        print("Pontuação final do jogador 1: \nPontuação final do jogador 2: \n")
         break
